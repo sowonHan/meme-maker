c3bc05e72423ebc874ae6e2988276c133a46a398
sowonHan <rippang781@gmail.com>
2023-08-23 16:33:12 +0900

    2023.08.23 완강

--- app.js
+++ app.js
@@ -11,12 +11,15 @@ const modeBtn = document.getElementById("mode-btn");
 const resetBtn = document.getElementById("reset-btn");
 const eraserBtn = document.getElementById("eraser-btn");
 const fileInput = document.getElementById("file");
+const textInput = document.getElementById("text");
+const saveBtn = document.getElementById("save");
 
 const CANVAS_WIDTH = 700;
 const CANVAS_HEIGHT = 700;
 canvas.width = CANVAS_WIDTH;
 canvas.height = CANVAS_HEIGHT;
 ctx.lineWidth = lineWidth.value;
+ctx.lineCap = "round";
 
 // Flat UI Colors에서 색 가져옴
 // const colors = [
@@ -147,11 +150,12 @@ function onEraserClick() {
 eraserBtn.addEventListener("click", onEraserClick);
 
 function onFileChange(event) {
+  // files가 배열 형태인 이유는 input에 multiple 속성을 넣을 수 있기 때문이다. multiple 속성을 해두면 여러 개의 파일을 업로드할 수 있다.
   const file = event.target.files[0];
   const url = URL.createObjectURL(file);
-  // console.log(url)을 찍었을 때 나오는 blob:http://127.0.0.1:5500/8084aa87-faa2-4405-be56-4aca94ae4793 <- 이것은 실제 인터넷에 존재하는 url이 아니고 브라우저를 위한 것. 브라우저가 자신의 메모리에 있는 파일을 드러내는 방식이다.
+  // console.log(url)을 찍었을 때 나오는 blob:http://127.0.0.1:5500/8084aa87-faa2-4405-be56-4aca94ae4793 <- 이것은 실제 인터넷에 존재하는 url이 아니고 파일을 업로드한 브라우저에서만 접근 가능. 브라우저 메모리에 저장된 파일을 URL을 통해 접근하는 것이다.
 
-  // new Image()는 html에서 img 태그를 생성하는 것과 기능적으로 완전히 동일하다.
+  // new Image()는 document.createElement("img")와 기능적으로 완전히 동일하다.
   const image = new Image();
   image.src = url;
   // 엘리먼트에 이벤트를 추가하는 방법으로 이렇게도 가능하다.
@@ -161,3 +165,41 @@ function onFileChange(event) {
   };
 }
 fileInput.addEventListener("change", onFileChange);
+
+// 텍스트 기능
+ctx.font = "30px serif";
+
+function onDoubleClick(event) {
+  /* strokeText()는 테두리 있고 안은 비어있는 글자를 만든다. 지정한 색은 테두리 색이 됨. strokeText만 하면 lineWidth에 영향을 받아서 글씨가 두꺼워진다. 그래서 save, restore 함수를 사용해 기존 설정을 저장&복구시킨 뒤 텍스트를 입력할 때 lineWidth 설정을 변경하는 식으로 한다.
+  fillText()은 색을 전부 채운 글자를 만든다. fillText는 lineWidth에 영향을 받지 않는다.
+  두 가지 메소드 전부 페인트통 모드일 때는 더블 클릭이 아니라 클릭 이벤트가 적용되어 텍스트 입력이 안 된다. 근데 강의에서는 이미지 위에 텍스트를 얹는 상황을 가정하고 만들기 때문에 여기까지는 신경 쓰지 않는 듯.
+   */
+  const text = textInput.value;
+
+  // strokeText로 할 경우 코드
+  // if (text !== "") {
+  //   // save : ctx의 색상, 스타일 등 현재 상태를 저장한다.
+  //   ctx.save();
+  //   ctx.lineWidth = 2;
+  //   ctx.strokeText(text, event.offsetX, event.offsetY);
+  //   // restore : 저장된 상태로 돌아간다.
+  //   ctx.restore();
+  // }
+
+  // fillText로 할 경우 코드
+  if (text !== "") {
+    ctx.fillText(text, event.offsetX, event.offsetY);
+  }
+}
+canvas.addEventListener("dblclick", onDoubleClick);
+
+function onSaveClick() {
+  // canvas.toDataURL : 캔버스에 있는 그림 데이터를 URL로 변환해주는 메소드. 결과값은 캔버스에 그린 그림을 문자열로 나타낸 것이다.
+  const url = canvas.toDataURL();
+  const a = document.createElement("a");
+  a.href = url;
+  // 앵커 태그의 download 속성은 브라우저에게 href에 링크된 콘텐츠를 브라우저에 표시하지 말고 다운로드하라고 지시하는 역할을 한다. download에 값을 줄 경우 기본 파일명을 지정할 수 있다.
+  a.download = "myDrawing.png";
+  a.click(); // 클릭 이벤트를 실행시키는 메소드
+}
+saveBtn.addEventListener("click", onSaveClick);
